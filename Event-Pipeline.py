5eb51b4649d5c5f12e5bfde028970fc6b42ad55a
Rice Tyler <rht6nf@virginia.edu>
2017-07-11 15:29:50 +0200

    parsing group fix

--- Event-Pipeline.py
+++ Event-Pipeline.py
@@ -34,15 +34,44 @@ class ParseEventFn(beam.DoFn):
 
   def process(self, element):
     attr = element.strip('{').strip("}").replace('"', "").split(",")
+    attr = i.strip('{').strip("}").replace('"', "").split(",")
+    ### dma fix
     if (attr[15].split(":")[1] != "null"):
-          attr[15] = attr[15] + attr[16]
-          attr.remove(attr[16])
+        while (':' not in attr[16]):
+            attr[15] = attr[15] + ',' + attr[16]
+            attr.remove(attr[16])
+    ### JSON sub-group-check
+    sub_group_check = 0
+    while (sub_group_check < len(attr)):
+        try:
+            if (len(attr[sub_group_check].split(':')) == 1):
+                attr[sub_group_check - 1] = attr[sub_group_check - 1] + "," + attr[sub_group_check]
+                attr.remove(attr[sub_group_check])
+                sub_group_check -= 1
+            sub_group_check += 1
+        except:
+            self.num_parse_error.inc()
+            logging.info('subgroup parse error on %s', i)
     attr_dict = {}
+    line_count = 0
     for k in attr:
-          print(k)
-          key_val = k.split(":", 1)
-          print(key_val)
-          attr_dict[key_val[0]] = key_val[1]
+        line_count += 1
+        try:
+            # if((len(k.split(':')) > 2) & ("user_properties" in k)):
+            #     key_val = k.replace("user_properties:{","",1).split(":")
+            # elif('event_properties' in k ) & (len(k.split(':')) > 2):
+            #     key_val = k.replace("event_properties:{","",1).split(":")
+            # else:
+            #     key_val = k.strip('\n').strip('}').strip('{').split(":",1)
+            if ((len(k.split(':')) > 2) & ("{" in k)):
+                key_val = k.replace('{', '', 1).split(':')
+                attr_dict[key_val[1]] = key_val[2]
+            else:
+                key_val = k.strip('\n').split(':')
+                attr_dict[key_val[0]] = key_val[1]
+        except:
+            self.num_parse_error.inc()
+            logging.info('split parse error on %s', k)
 
     try:
       anonymous_id = attr_dict[""].strip()
@@ -50,12 +79,12 @@ class ParseEventFn(beam.DoFn):
       context_2 = attr_dict[""].strip()
       context_3 = attr_dict[""].strip()
       context_4 = attr_dict[""].strip()
-      event = attr_dict[""].strip()
-      event_text = attr_dict[""].strip()
-      id = attr_dict[""].strip()
-      sent_at = attr_dict[""].strip()
-      received_at = attr_dict[""].strip()
-      user_id = attr_dict[""].strip()
+      event = attr_dict["event_id"].strip()
+      event_text = attr_dict["event_type"].strip()
+      id = attr_dict["uuid"].strip()
+      sent_at = attr_dict["client_upload_time"].strip()
+      received_at = attr_dict["server_upload_time"].strip()
+      user_id = attr_dict["user_id"].strip()
       yield {'anonymous_id': anonymous_id, 'context_1': context_1, 'context_2': context_2,
              'context_3' : context_3, 'context_4' : context_4, 'event':event,
              'event_text' : event_text,'id': id,'sent_at' : sent_at,'received_at':received_at,
@@ -72,12 +101,12 @@ def configure_bigquery_write():
       ('context_2', 'STRING', lambda e: e[2]),
       ('context_3', 'STRING', lambda e: e[3]),
       ('context_4', 'STRING', lambda e: e[4]),##Context field placeholders ^^
-      ('event', 'STRING', lambda e: e[1]),
-      ('event_text', 'STRING', lambda e: e[1]),
-      ('id', 'STRING', lambda e: e[1]),
-      ('received_at', 'STRING', lambda e: e[1]),
-      ('sent_at', 'STRING', lambda e: e[1]),
-      ('user_id', 'STRING', lambda e: e[1]),
+      ('event', 'STRING', lambda e: e[5]),
+      ('event_text', 'STRING', lambda e: e[6]),
+      ('id', 'STRING', lambda e: e[7]),
+      ('received_at', 'STRING', lambda e: e[8]),
+      ('sent_at', 'STRING', lambda e: e[9]),
+      ('user_id', 'STRING', lambda e: e[10]),
   ]
 
 
--- json-parse-test.py
+++ json-parse-test.py
@@ -1,16 +1,51 @@
 import json
-file = open("C:/Users/Student/Documents/Over/parse","r")
+import apache_beam as beam
+from apache_beam.metrics import Metrics
+
+self = 'l'
+self.num_parse_error = Metrics.counter(self.__class__,'num parse errors')
+file = open("C:/Users/Student/Documents/Over/2017-06-28_648","r")
+
 file_read = file.readlines()
+events = {}
 for i in file_read:
     attr = i.strip('{').strip("}").replace('"',"").split(",")
+    ### dma fix
     if(attr[15].split(":")[1] != "null"):
-        attr[15] =attr[15] + attr[16]
-        attr.remove(attr[16])
+        while(':' not in attr[16]):
+            attr[15] = attr[15] + ',' + attr[16]
+            attr.remove(attr[16])
+    ### JSON sub-group-check
+    sub_group_check = 0
+    while(sub_group_check < len(attr)):
+        try:
+            if(len(attr[sub_group_check].split(':')) == 1):
+                attr[sub_group_check-1] = attr[sub_group_check-1]+ ","  + attr[sub_group_check]
+                attr.remove(attr[sub_group_check])
+                sub_group_check-=1
+            sub_group_check+=1
+        except:
+            self.num_parse_error.inc()
+            logging.info('subgroup parse error on %s',i)
     attr_dict ={}
+    line_count = 0
     for k in attr:
-        print(k)
-        key_val = k.split(":",1)
-        print(key_val)
-        attr_dict[key_val[0]] = key_val[1]
-    print(attr_dict)
+        line_count +=1
+        try:
+            # if((len(k.split(':')) > 2) & ("user_properties" in k)):
+            #     key_val = k.replace("user_properties:{","",1).split(":")
+            # elif('event_properties' in k ) & (len(k.split(':')) > 2):
+            #     key_val = k.replace("event_properties:{","",1).split(":")
+            # else:
+            #     key_val = k.strip('\n').strip('}').strip('{').split(":",1)
+            if((len(k.split(':')) > 2) & ("{" in k)):
+                key_val = k.replace('{', '', 1).split(':')
+                attr_dict[key_val[1]] = key_val[2]
+            else:
+                key_val = k.strip('\n').split(':')
+                attr_dict[key_val[0]] = key_val[1]
+        except:
+            self.num_parse_error.inc()
+            logging.info('split parse error on %s', k)
+
 
